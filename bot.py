ee51f91fdb2539c57310ff36b25c8ad118a08b06
vpasspro <vpassprosolution@gmail.com>
2025-03-07 21:25:26 +0800

    fix

--- bot.py
+++ bot.py
@@ -72,9 +72,9 @@ async def main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await query.message.edit_text(
-        "*WELCOME  TO  VPASS PRO  VERSION TWO* \n"
+        "*WELCOME  TO  VPASS PRO  VERSION  TWO* \n"
         "    The Future of Intelligent Starts Here\n"
-        "          *CHOOSE YOUR STRATEGY*",
+        "            *CHOOSE YOUR STRATEGY*",
         parse_mode="MarkdownV2",
         reply_markup=reply_markup
     )
